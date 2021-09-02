538326b9271dd6435af8a018fca416adfb1068c9
Crowdbotics <team@crowdbotics.com>
2021-09-02 02:24:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "muddy_cloud_30262Identifier",
+  url: "https://muddy_cloud_30262Identifier.botics.co",
+  api: "https://muddy_cloud_30262Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
